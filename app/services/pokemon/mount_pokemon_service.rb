6830fa15430fa8baea28112a16a50611cf4c686e
riulsudario <riulsudario@jera.com.br>
2021-02-23 23:50:57 -0400

    [UPDATE] updating object

--- app/services/pokemon/mount_pokemon_service.rb
+++ app/services/pokemon/mount_pokemon_service.rb
@@ -1,10 +1,14 @@
 class Pokemon::MountPokemonService < BusinessProcess::Base
+  STEPS = 5
+
   needs :pokemon
 
   steps :init_variables,
         :mount_abilities,
         :name,
         :photo,
+        :stats,
+        :max_base_stat,
         :mount_pokemon
 
   def call
@@ -16,6 +20,7 @@ class Pokemon::MountPokemonService < BusinessProcess::Base
 
   def init_variables
     @abilities = []
+    @stats = []
   end
 
   def mount_abilities
@@ -30,18 +35,37 @@ class Pokemon::MountPokemonService < BusinessProcess::Base
   end
 
   def name
-    @name = pokemon['name']
+    @name = pokemon['name'].humanize
   end
 
   def photo
     @photo = pokemon['sprites']['other']['official-artwork']['front_default']
   end
 
+  def stats
+    @stats = pokemon['stats'].map do |stat|
+      {
+        name: stat['stat']['name'].humanize,
+        base_stat: stat['base_stat']
+      }
+    end
+  end
+
+  def max_base_stat
+    @max_base_stat = @stats.map { |d| d[:base_stat] }.max
+  end
+
   def mount_pokemon
     @pokemon = {
+      id: pokemon['id'],
       name: @name,
       abilities: @abilities,
-      photo: @photo
+      photo: @photo,
+      chart: {
+        stats: @stats,
+        max_base_stat: @max_base_stat,
+        step_size: @max_base_stat / STEPS
+      }
     }
   end
 end
